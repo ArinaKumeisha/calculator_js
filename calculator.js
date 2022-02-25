1861a0ecf941a8354817ac188e5f513828a034fc
ArinaKumeisha <arinanozdracheva@mail.ru>
2022-02-26 00:35:46 +0300

    logic

---
+++ calculator.js
@@ -0,0 +1,87 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+const action = ['-', '+', 'x', '/']
+
+// display
+let out = document.querySelector('.calc-screen p');
+
+function clearAll() {
+  a = '';
+  b = '';
+  sign = '';
+  finish = false;
+  out.textContent = '';
+}
+
+const ac = document.querySelector('.ac')
+
+ac.onclick = clearAll;
+
+
+const buttons = document.querySelector('.buttons')
+buttons.onclick = (event) => {
+  if (!event.target.classList.contains('btn')) return; //нажата не кнопка
+  if (event.target.classList.contains('ac')) return;// нажата кнопка ac
+  
+  out.textContent = '';
+  
+  const key = event.target.textContent; //получаю нажатую кнопку
+  
+  if (digit.includes(key)) {
+    if (b === '' && sign === '') {
+      a += key;
+      console.log(a, b, sign)
+      out.textContent = a;
+    } else if (a !== '' && b !== '' && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    } else {
+      b += key
+      out.textContent = b;
+      console.log(a, b, sign)
+    }
+    return;
+    
+  }
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.log(a, b, sign)
+  }
+  if (key === '=') {
+    if(b === '') b = a;
+    
+    switch (sign) {
+      case '+':
+        a = (+a) + (+b);
+        break;
+        
+      case '-':
+        a = a - b;
+        break;
+        
+      case 'x':
+        a = a * b;
+        break;
+        
+      case '/':
+        if(b === '0'){
+         out.textContent = 'Ошибка';
+         a = '';
+         b = '';
+         sign = '';
+         return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+    
+  }
+}
